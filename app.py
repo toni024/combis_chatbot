0ecdd34dcbcd3854866763e0cbaa4f2e7c30dad9
Antonio <toni.bosak@gmail.com>
2017-04-08 22:04:56 +0200

    asdf

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 # @app.route('/<path:path>')
 # def static_file(path):
 #     return app.send_static_file(path)
@@ -5,7 +8,11 @@
 from flask import Flask, send_from_directory, request
 import json
 import requests
-import logging
+import google_helpers
+import urllib
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 
 # app = Flask(__name__, static_url_path='/build')
@@ -23,52 +30,118 @@ def index():
     return send_from_directory('build/', 'index.html')
 
 
-@app.route("/send/<text>", methods=['GET', 'POST'])
-def senda(text):
-    req = json.dumps(sendToBot(text).json())
-    return req
-
-
-witToken = "XGCQNQXGXO6DMGQGPNMYJ2C4HZNKBF5Z"
-
-
 @app.route("/check", methods=['POST', 'GET'])
 def check():
-            # {"entities": {}, "msg_id": "53ebc318-bded-421c-85b3-6bb74a5b01ac", "_text": "bok"}
-# 172.16.216.58 - - [08/Apr/2017 16:35:22] "POST /check HTTP/1.1" 200 -
-# INFO:werkzeug:172.16.216.58 - - [08/Apr/2017 16:35:22] "POST /check HTTP/1.1" 200 -
-# text read: kaj kurac
-# {"entities": {"intent": [{"confidence": 0.5890985834213512, "value": "bezobraznik"}], "location": [{"suggested": true, "confidence": 0.9630074209263095, "type": "value", "value": "kaj kurac"}]}, "msg_id": "bd6a6a48-173a-48b7-9e97-71216d129691", "_text": "kaj kurac"}
-    
     # post data message, longitute, latitude
     if request.method == 'GET':
         return json.dumps({'text': 'hello'})
     else:
-        text = json.loads(request.data)
-        print("text read: "+text['message'])
-        #datae = json.dumps(sendToBot(text['message']))
-        datae = sendToBot(text['message'])
-        intent = getIntent(datae)
-        
-        print(datae)
-        print(datae.get("entities").get("intent")[0])
-        print(type(json.dumps({'text': datae.get("entities").get("intent")})))
-        return json.dumps({'text': datae.get("entities").get("intent")[0]})
+        data = request.data
+        text = json.loads(data)['message']
+        data = json.loads(data)
+        data['longitude'] = '15'
+        data['latitude'] = '45'
+        text = text.encode('utf-8')
+        log.debug("text read: "+text)
 
+        
+        # text = urllib.quote_plus(text)
+        # log.debug("text after quote plus: "+text)
+
+        english = google_helpers.translate_to_english(text)
+        text = english.get("text")[0]
+
+        # a = {'text': text, 'to': 'en'}
+        
+        # english = requests.get("http://www.transltr.org/api/translate?"+urllib.urlencode(a))
+        # # #english =
+
+        
+        # english = english.json()
+        #text = english.get("translationText")
+
+        log.debug("enblish translation: " + text)
+        datae = sendToBot(text)
+        log.debug(json.dumps(datae))
+        intent = getIntent(datae)
+        if intent is None:
+            return json.dumps({"text": "null"})
+
+        # brute force FTW !
+        if intent == "hospital":
+            return google_helpers.fetch_google_places({
+                'latitude': data['latitude'],
+                'longitude': data['longitude'],
+                'type': intent})
+        elif intent == "food":
+            return google_helpers.fetch_google_places({
+                'latitude': data['latitude'],
+                'longitude': data['longitude'],
+                'type': 'restaurant'})
+        elif intent == "drink":
+            return google_helpers.fetch_google_places({
+                'latitude': data['latitude'],
+                'longitude': data['longitude'],
+                'type': 'cafe'})
+        elif intent == "nightlife":
+            return google_helpers.fetch_google_places({
+                'latitude': data['latitude'],
+                'longitude': data['longitude'],
+                'type': 'night_club'})
+        elif intent == "accomodation":
+            return google_helpers.fetch_google_places({
+                'latitude': data['latitude'],
+                'longitude': data['longitude'],
+                'type': 'lodging'})
+        elif intent == "siteseeing":
+            return google_helpers.fetch_google_places({
+                'latitude': data['latitude'],
+                'longitude': data['longitude'],
+                'type': 'museum|casino'})
+        elif intent == "police":
+            return google_helpers.fetch_google_places({
+                'latitude': data['latitude'],
+                'longitude': data['longitude'],
+                'type': intent})
+        elif intent == "atm":
+            return google_helpers.fetch_google_places({
+                'latitude': data['latitude'],
+                'longitude': data['longitude'],
+                'type': intent})
+        elif intent == "greeting":
+            return json.dumps({'text': 'hi'})
+        elif intent == "weather":
+            pass
+
+        log.debug("intent retuned: " + intent)
+        return json.dumps({'text': intent})
 
 
 def getIntent(data):
-    temp = datae.get("entities").get("intent")[0]
+    # print(datae)
+    # print(datae.get("entities").get("intent")[0])
+    # print(type(json.dumps({'text': datae.get("entities").get("intent")})))
+    temp = data.get("entities").get("intent")
+    if temp is None:
+        log.debug("getIntent is None")
+        return None
+    else:
+        log.debug("getIntent: " + json.dumps(temp))
+        return temp[0].get("value")
 
 
 def sendToBot(text):
+    witToken = "XGCQNQXGXO6DMGQGPNMYJ2C4HZNKBF5Z"
     text = text.replace(" ", "+")
     header = {'Authorization': 'Bearer '+witToken}
-    logging.debug("text for bot {0}".format(text))
+    log.debug("text for bot {0}".format(text))
     return requests.get("https://api.wit.ai/message?v=20160526&q="+text,
                         headers=header).json()
 
+log = None
 
 if __name__ == '__main__':
+    google_helpers.log = app.logger
+    log = app.logger
     app.debug = debug
     app.run(host='0.0.0.0')
