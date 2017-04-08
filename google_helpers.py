89677172124bffb4a414ef340e9fe3c67356206d
acabraja <ante.cabraja@slsolucije.hr>
2017-04-08 23:42:21 +0200

    Merge branch 'master' of /home/acabraja/Documents/combis-app/combis_chatbot with conflicts.

--- google_helpers.py
+++ google_helpers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import logging
 
+import logging
 import requests
 import sys
 import json
@@ -54,6 +54,15 @@ def translate_to_english(sentence):
     return response.json()
 
 
+def translate_to_base_lang(sentence, lang):
+    encoded_sentence = urllib.quote_plus(sentence)
+    key = 'trnsl.1.1.20170408T140154Z.8efdaac6447952eb.3511edc0fcd5a38e5b92f9ec1a91266d894c1943'
+    url = 'https://translate.yandex.net/api/v1.5/tr.json/translate?key=' + key + '&lang=en-'+lang+'&text=' + encoded_sentence
+    response = requests.get(url)
+    log.info('test')
+    return response.json()
+
+
 def get_weather_forecast(args, latitude=None, longitude=None):
     json_args = json.loads(args)
     if json_args.get('latitude'):
@@ -64,15 +73,19 @@ def get_weather_forecast(args, latitude=None, longitude=None):
     url = 'http://api.openweathermap.org/data/2.5/forecast/daily?lat='+latitude+'&lon='+longitude+'&APPID='+key
     response = requests.get(url)
     forecast = response.json().get('list')
-    print(forecast[0])
-    return response.json()
-
+    ret_arr = []
+    for day in forecast:
+        item = {
+            'temp': round(day.get('temp').get('day') - 273.15),
+            'description': day.get('weather')[0].get('main')
+        }
+        ret_arr.append(item)
+    return ret_arr
 
 
 if __name__ == "__main__":
     #args = sys.argv[1]
-    #print(fetch_google_places('{"latitude":"45.815399","longitude":"15.966568","type":"restaurant","radius":"5000"}'))
-    #print(translate_to_english("kakvo je vrijeme u splitu"))
-#    a =
-    print(fetch_google_places(' {"latitude": "15.2384", "longitude": "45.1235234", "type": "restaurant"}'))
-    # print(get_weather_forecast('{"latitude":"45.815399","longitude":"15.966568"}'))
+    print(fetch_google_places('{"latitude":"45.815399","longitude":"15.966568","type":"sightseeing"}'))
+    #print(translate_to_english("đe kafana"))
+    #print(get_weather_forecast('{"latitude":"45.815399","longitude":"15.966568"}'))
+    #print(translate_to_base_lang("we are in the very hell of a position", "hr"))
