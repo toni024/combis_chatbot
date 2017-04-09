bb8f7a8f43d9579386139f5a5d7bcc1d334e749d
ma_mihili <markomihelj@gmail.com>
2017-04-09 03:40:11 +0200

    ajde malo i od mene

--- reactJS/src/components/Chat.js
+++ reactJS/src/components/Chat.js
@@ -12,6 +12,7 @@ class Chat extends React.Component {
         super(props)
         this.state = {
             msg_txt: "",
+            date: new Date(),
         }
     }
 
@@ -28,7 +29,7 @@ class Chat extends React.Component {
             scrollTop: $(".chaty")[0].scrollHeight
         }, 100)
         if (e.key === "Enter")
-            this.props.addMessage(this.state.msg_txt)
+            this.props.addMessage(this.state.msg_txt, this.props.chat.lon, this.props.chat.lat)
     }
 
     render() {
@@ -46,23 +47,43 @@ class Chat extends React.Component {
                             <div>
                                 {this.props.chat.messages.map((item, i) => (
                                     <div key={i} >
-                                        {item.id === "user" ?
-                                            < div className="bubble me" >
-                                                <div>
-                                                    {console.log("aaaa", !(item.message.weather))}
-                                                    {!item.message.google_maps ? "" :
+                                        {!(item.id === "user") ?
+                                            < div  > {/*bot*/}
+                                                {item.message.google_maps ? <div>
+                                                    {typeof item.message.google_maps != "object" ? "" :
                                                         < div >
-                                                            {item.message.google_maps.link ? <img src={item.message.google_maps.link} alt="" /> : ""}
-                                                            {item.message.google_maps.name ? <div>{item.message.google_maps.name}</div> : ""}
-                                                            {item.message.google_maps.address ? <div>{item.message.google_maps.address}</div> : ""}
+                                                            {item.message.google_maps.length > 0 ? <div>
+                                                                {item.message.google_maps.map((place, i) => (
+                                                                    <div className="bubble me">
+                                                                        <img src={place.link} alt="" />
+                                                                        <h3>{place.name}</h3>
+                                                                        {place.address}
+                                                                        <br />
+                                                                    </div>
+                                                                ))}
+                                                            </div> : ""}
                                                         </div>}
-                                                    {!(item.message.weather) ? "" :
-                                                        <div>
-                                                            {console.log("ssss", item.message.weather)}
-                                                            {item.message.weather.length > 0 ? <div>{item.message.weather[0].temp} ℃</div> : "asd"}
-                                                            {item.message.weather.length > 0 ? <div>{item.message.weather[0].desc}</div> : "asds"}
-                                                        </div>}
-                                                    {item.message.text ? <div>{item.message.text}</div> : ""}
+                                                </div> : ""}
+                                                <div>
+                                                    {item.message.weather ? <div>
+                                                        {typeof item.message.weather != "object" ? "" :
+                                                            <div >
+                                                                {item.message.weather.length > 0 ? <div>
+                                                                    {item.message.weather.map((day, i) => (
+                                                                        <div className="bubble me">
+                                                                            {(this.state.date.getDate() + i) > 9 ?
+                                                                                (this.state.date.getDate() + i) : "0" + (this.state.date.getDate() + i)}.
+                                                                                {this.state.date.getMonth() > 9 ? this.state.date.getMonth() : "0" + this.state.date.getMonth()}
+                                                                            <i className={day.i}></i>
+                                                                            {day.temp}℃ {day.description}
+                                                                        </div>
+                                                                    ))}
+                                                                </div> : ""}
+                                                            </div>}
+                                                    </div> : ""}
+                                                    {item.message.text ? <div >
+                                                        {item.message.text != "null" ? <div className="bubble me">{item.message.text}</div> : " "}
+                                                    </div> : ""}
                                                 </div>
                                             </div> :
                                             <div className="bubble you">
